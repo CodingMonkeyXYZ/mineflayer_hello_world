e2cd3ceb1b111670ba0db8d51552922794fc833c
Alex <alex@wellbalanceddesign.com>
2015-05-20 12:46:29 +0100

    got chat and whisper working for civcraft

--- echo.js
+++ echo.js
@@ -1,24 +1,49 @@
 var mineflayer = require('mineflayer');
 var argv = require('minimist')(process.argv.slice(2));
 
-console.log("Starting bot.");
-
-console.log(argv.host);
-console.log(argv.port);
-console.log(argv.username);
-console.log(argv.password);
-console.log("--------------");
-
 var bot = mineflayer.createBot({
-  host: argv.host, // optional
-  port: argv.port,       // optional
-  username: argv.username, // email and password are required only for
-  password: argv.password,          // online-mode=true servers
+  host: argv.host,
+  port: argv.port,
+  username: argv.username,
+  password: argv.password,
 });
-bot.on('chat', function(username, message) {
-  if (username === bot.username) return;
+
+//clear the vanilla patterns from the list to scan. V7+ all use json anyway.
+//bot.chatPatterns = [];
+
+bot.chatAddPattern(/^([a-zA-Z0-9_]{1,16}):\s(.+)/, "chat", "CivCraft chat");
+bot.chatAddPattern(/^From\s(.+):\s(.+)/, "whisper", "CivCraft pm");
+//bot.chatAddPattern(/^(.*)$/, "chat", "CivCraft chat");
+
+//TODO: it's picking up 'hi' as the wisper username and not getting a message.
+
+
+bot.on('chat', function(username, message) {  
+  if (username === bot.username) return;  
   bot.chat(message);
 });
+
+bot.on('whisper', function(username, message) {
+  console.log("username: "+username);
+  console.log("whisper:"+message);
+  if (username === bot.username) return;  
+  bot.chat("/r "+message);
+});
+
 bot.on('error', function(error) {
   console.error(error.stack);
+});
+
+bot.on('spawn', function() {
+  console.log("spawn.");
+  bot.chat("I have awoken.");
+});
+
+bot.on('kicked', function(reason) {
+  console.log("We were kicked, because:");
+  console.error(reason);
+});
+
+bot.on('health', function() {
+  console.warn("Health change: "+bot.health);
 });
